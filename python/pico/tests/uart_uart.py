c34a52a872eee88c77431a9b247421f3cd66cf01
Jeff Beman <jbeman@hotmail.com>
2023-11-22 22:32:58 -0800

    added getting started instructions

---
+++ python/pico/tests/uart_uart.py
@@ -0,0 +1,24 @@
+import config
+import kineticClock
+
+#picos must have common ground for uart to work
+def main():
+    conf = config.Config("conf.json")
+    clock = kineticClock.kineticClock(conf)
+    
+    try:
+        extend = clock.getExtendAngles(1)
+        print("getExtendAngles(1)")
+        print(extend)
+
+        retract = clock.getRetractAngles(1)
+        print("getRetractAngles(1)")
+        print(retract)
+    except KeyboardInterrupt:
+        print('KeyboardInterrupt')
+    finally:
+        print('Done')
+    
+
+if __name__ == "__main__":
+    main()
